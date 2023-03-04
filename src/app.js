686024372c4492bb9cbff76fd9a7b71a16960b9b
Németh Ottó <ottoburg17@gmail.com>
2023-03-04 10:27:18 +0100

    app.js kész

--- src/app.js
+++ src/app.js
@@ -7,3 +7,37 @@
 * Github: https://github.com/nemethotto/
 * Licenc: GNU GPL
 */ 
+
+const tbody = document.querySelector('#tbody');
+var emplist = [
+    { name: 'Cassis', wheel: '28', use: 'offroad', price: '557900' },
+    { name: 'Albion 900', wheel: '28', use: 'trekking', price: '519900'},
+    { name: 'Asgard', wheel: '29', use: 'technikás utak', price: '519900'},
+    { name: 'Ruga', wheel: '29', use: 'hegyi', price: '372900'},
+    { name: 'Reptila', wheel: '28', use: 'városi', price: '308900'},
+    { name: 'Sirmium', wheel: '29', use: 'hegyi', price: '264900'}
+];
+
+function createTable() {
+    empList.forEach((emp) => {
+        console.log(emp.name)
+        let tr = document.createElement('tr');
+
+        let tdName = document.createElement('td');
+        let tdWheel = document.createElement('td');
+        let tdUse = document.createElement('td');
+        let tdPrice = document.createElement('td');
+
+        tdName.textContent = emp.name;
+        tdWheel.textContent = emp.wheel;
+        tdUse.textContent = emp.use;
+        tdPrice.textContent = emp.price;
+
+        tbody.append(tr);
+        tr.append(tdName);
+        tr.append(tdWheel);
+        tr.append(tdUse);
+        tr.append(tdPrice);
+    });
+};
+createTable();
