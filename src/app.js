78505eb6a49c9f3940e28b51d5b0996b4eb0aa00
Németh Ottó <ottoburg17@gmail.com>
2023-03-04 10:52:29 +0100

    app.js javítva

--- src/app.js
+++ src/app.js
@@ -9,13 +9,13 @@
 */ 
 
 const tbody = document.querySelector('#tbody');
-var emplist = [
-    { name: 'Cassis', wheel: '28', use: 'offroad', price: '557900' },
-    { name: 'Albion 900', wheel: '28', use: 'trekking', price: '519900'},
-    { name: 'Asgard', wheel: '29', use: 'technikás utak', price: '519900'},
-    { name: 'Ruga', wheel: '29', use: 'hegyi', price: '372900'},
-    { name: 'Reptila', wheel: '28', use: 'városi', price: '308900'},
-    { name: 'Sirmium', wheel: '29', use: 'hegyi', price: '264900'}
+var empList = [
+    {id: 1, name: 'Cassis', wheel: '28', use: 'offroad', price: '557900' },
+    {id: 2, name: 'Albion 900', wheel: '28', use: 'trekking', price: '519900'},
+    {id: 3, name: 'Asgard', wheel: '29', use: 'technikás utak', price: '519900'},
+    {id: 4, name: 'Ruga', wheel: '29', use: 'hegyi', price: '372900'},
+    {id: 5, name: 'Reptila', wheel: '28', use: 'városi', price: '308900'},
+    {id: 6, name: 'Sirmium', wheel: '29', use: 'hegyi', price: '264900'}
 ];
 
 function createTable() {
@@ -23,17 +23,20 @@ function createTable() {
         console.log(emp.name)
         let tr = document.createElement('tr');
 
+        let tdId = document.createElement('td');
         let tdName = document.createElement('td');
         let tdWheel = document.createElement('td');
         let tdUse = document.createElement('td');
         let tdPrice = document.createElement('td');
-
+        
+        tdId.textContent = emp.id;
         tdName.textContent = emp.name;
         tdWheel.textContent = emp.wheel;
         tdUse.textContent = emp.use;
         tdPrice.textContent = emp.price;
 
         tbody.append(tr);
+        tr.append(tdId);
         tr.append(tdName);
         tr.append(tdWheel);
         tr.append(tdUse);
